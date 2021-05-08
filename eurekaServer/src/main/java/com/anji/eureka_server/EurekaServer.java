e1959eccc415ca539dc8d2f5a9d9b233b9456c8c
admin <admin@AJZN-TC>
2021-05-08 16:46:55 +0800

    fist

--- eurekaServer/src/main/java/com/anji/eureka_server/EurekaServer.java
+++ eurekaServer/src/main/java/com/anji/eureka_server/EurekaServer.java
@@ -1,7 +1,6 @@
 package com.anji.eureka_server;
 
 
-import com.netflix.discovery.shared.Application;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,6 +11,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServer {
 
     public static void main(String[] args){
-        new SpringApplicationBuilder(Application.class).web(WebApplicationType.REACTIVE).run(args);
+        new SpringApplicationBuilder(EurekaServer.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
